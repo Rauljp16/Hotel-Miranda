2d116f29130550b68c1aad51ffd0fbdb1716d9e7
Rauljp16 <raul_astra16@hotmail.com>
2024-06-16 01:01:14 +0200

    slider food pag. home

--- swiper.js
+++ swiper.js
@@ -14,9 +14,35 @@ const swiper = new Swiper(".swiper", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+  // Navigation arrows
 
   // And if we need scrollbar
   scrollbar: {
     el: ".swiper-scrollbar",
   },
 });
+
+const swiper2 = new Swiper(".swiper2", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination2",
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next2",
+    prevEl: ".swiper-button-prev2",
+  },
+  // Navigation arrows
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar2",
+  },
+});
+
