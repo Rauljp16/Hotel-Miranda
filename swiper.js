b151af767428e91b77056439a8980b17fb304714
Rauljp16 <raul_astra16@hotmail.com>
2024-07-25 22:36:27 +0200

    refactor: pagina home sin terminar

--- swiper.js
+++ swiper.js
@@ -21,3 +21,26 @@ const swiper = new Swiper(".swiper", {
     el: ".swiper-scrollbar",
   },
 });
+const swiper2 = new Swiper(".swiper2", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  // Navigation arrows
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
