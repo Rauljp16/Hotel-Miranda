6b56f7f25fb5aeb14154757eb63ecb8d4d8dec02
Rauljp16 <raul_astra16@hotmail.com>
2024-07-25 20:49:09 +0200

    terminando sliders

--- swiper.js
+++ swiper.js
@@ -21,28 +21,3 @@ const swiper = new Swiper(".swiper", {
     el: ".swiper-scrollbar",
   },
 });
-
-const swiper2 = new Swiper(".swiper2", {
-  // Optional parameters
-  direction: "horizontal",
-  loop: true,
-
-  // If we need pagination
-  pagination: {
-    el: ".swiper-pagination2",
-    clickable: true,
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next2",
-    prevEl: ".swiper-button-prev2",
-  },
-  // Navigation arrows
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: ".swiper-scrollbar2",
-  },
-});
-
